1bc76deae7e909a9bbd75c9cdab42eca81356f98
zied <zied.ben.hammouda.ept@gmail.com>
2019-06-22 00:48:22 +0200

    initiate project

---
+++ src/main/java/com/phenix/Main.java
@@ -0,0 +1,14 @@
+package com.phenix;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world !");
+	}
+
+}
